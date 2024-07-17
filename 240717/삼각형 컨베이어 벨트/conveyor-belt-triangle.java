69560804c40c0e5233640e2e5d7a540766b67911
개 <163699743+EatRawLife@users.noreply.github.com>
2024-07-17 15:57:31 +0900

    Title: 삼각형 컨베이어 벨트, Time: 151ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240717/삼각형 컨베이어 벨트/conveyor-belt-triangle.java
+++ 240717/삼각형 컨베이어 벨트/conveyor-belt-triangle.java
@@ -5,16 +5,16 @@ public class Main{
         int n = scan.nextInt();
         int t = scan.nextInt();
 
-        int[][] array = new int[n][n];
-        for(int i = 0; i< n; i++){
+        int[][] array = new int[3][n];
+        for(int i = 0; i< 3; i++){
             for(int j = 0; j< n; j++){
                 array[i][j] = scan.nextInt();
             }
         }
 
-        int temp = array[n-1][n-1];
+        int temp = array[2][n-1];
         for(int j = 0; j< t; j++){
-        for(int z = n-1; z>=0; z--){
+        for(int z = 2; z>=0; z--){
             for(int i = n-1; i>0;i-- ){
                 array[z][i] = array[z][i-1];
             }
@@ -26,7 +26,7 @@ public class Main{
         }
         }
 
-        for(int i = 0; i<n; i++){
+        for(int i = 0; i<3; i++){
             for(int j = 0; j< n; j++){
                 System.out.print(array[i][j]+" ");
             }
