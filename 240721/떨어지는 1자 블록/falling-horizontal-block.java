f8018aa6aca0d6268b570ab6da55dcefdb05e995
개 <163699743+EatRawLife@users.noreply.github.com>
2024-07-21 23:45:29 +0900

    Title: 떨어지는 1자 블록, Time: 359ms, Memory: 22MB, Status: Wrong Answer - CodeTree

--- 240721/떨어지는 1자 블록/falling-horizontal-block.java
+++ 240721/떨어지는 1자 블록/falling-horizontal-block.java
@@ -57,7 +57,7 @@ public class Main {
        
 
         for(int i = 0; i< n+1; i++){
-            if(i == n+1){
+            if(i == n){
                 ctinue = false;
             }
             else
