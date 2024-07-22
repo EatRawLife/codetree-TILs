ffd130c8d34d240ec616a4769aa0ee444b434fdd
개 <163699743+EatRawLife@users.noreply.github.com>
2024-07-22 20:10:39 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 1693ms, Memory: 36MB, Status: Passed - CodeTree

---
+++ 240722/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.java
@@ -0,0 +1,33 @@
+//k가 입력, 인자로count 전달해서 인자가 k가 되면 실행. 마지막에는 pop.
+
+import java.util.*;
+public class Main {
+    public static int n;
+    public static int k;
+
+    public static Stack<Integer> st = new Stack<Integer>();
+    public static void pick(int count){
+        for(int i= 1; i<= n; i++){
+            st.push(i);
+            if(count == k){
+                for(int j = 0; j<k; j++){
+                    System.out.print(st.get(j)+ " ");
+                }
+                System.out.println();
+                st.pop();
+                continue;
+            }
+            pick(count+1);
+            st.pop();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        n= scan.nextInt();
+        k = scan.nextInt();
+
+        pick(1);
+        
+    }
+}
